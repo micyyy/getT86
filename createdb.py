a86324cb36cab1d11009602c95f6a5ff3cbef07e
michael yeung <micyeung@gmail.com>
2017-12-08 17:33:50 +0800

    新增

---
+++ createdb.py
@@ -0,0 +1,42 @@
+﻿import sqlite3
+
+
+def create_inv():
+    conn = sqlite3.connect('inv.db')
+
+    c = conn.cursor()
+
+
+    c.execute('''CREATE TABLE INV
+        (ID INTEGER PRIMARY KEY AUTOINCREMENT,
+        EXCHANGE TEXT NOT NULL,
+        DATE TEXT NOT NULL,
+        CODE TEXT NOT NULL,
+        FI INTEGER,
+        IT INTEGER,
+        DL INTEGER,
+        TOTAL INTEGER);''')
+
+    conn.commit()
+
+def create_daily():    
+    conn = sqlite3.connect('daily.db')
+
+    c = conn.cursor()
+
+    c.execute('''CREATE TABLE DAILY
+        (ID INTEGER PRIMARY KEY AUTOINCREMENT,
+        EXCHANGE TEXT NOT NULL,
+        DATE TEXT NOT NULL,
+        CODE TEXT NOT NULL,
+        OPEN INTEGER,
+        HIGH INTEGER,
+        LOW INTEGER,
+        CLOSE INTEGER,
+        VOLUME INTEGER);''')
+        
+    conn.commit()
+
+if __name__ == '__main__':
+    create_inv()
+    create_daily()
