c4de9cfc2df9764d2b0f709ab77d4821a0728397
michael yeung <micyeung@gmail.com>
2017-12-08 17:44:03 +0800

    修改days 為 count, 並變為 int

--- plot_last_value.py
+++ plot_last_value.py
@@ -4,10 +4,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 #import matplotlib.font_manager as fm
 
-def main(code, days):    
+def main(code, count):    
     conn = sqlite3.connect('inv.db')
 
-    sql = "SELECT date, total from INV where code = '{}' order by date desc LIMIT {};".format(code, days)
+    sql = "SELECT date, total from INV where code = '{}' order by date desc LIMIT {};".format(code, count)
     df = pd.read_sql_query(sql, conn)
     
     df.sort_values('DATE', ascending = True, inplace=True)
@@ -16,7 +16,10 @@ def main(code, days):
         print('no data')
         return
     
-    my_title = '{} 近50天三大法人買賣超'.format(code)
+    if len(df) < count:
+        count = len(df)
+    
+    my_title = '{} 近{}天三大法人買賣超'.format(code, count)
     df.plot(kind = 'bar', title = my_title)
 
     plt.show()
@@ -24,9 +27,9 @@ def main(code, days):
 if __name__ == '__main__':
     if len(sys.argv) >= 3:
         code = sys.argv[1]  
-        days = sys.argv[2]
+        count = int(sys.argv[2])
     else:
-        print('Usage: {} CODE DAYS'.format(sys.argv[0]))
+        print('Usage: {} CODE COUNT'.format(sys.argv[0]))
         exit()        
     
-    main(code, days)
+    main(code, count)
