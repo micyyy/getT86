42458336ef3544675f47b593709570e54e01d5be
michael yeung <micyeung@gmail.com>
2017-12-11 17:31:29 +0800

    新增

---
+++ query_bot_by_date.py
@@ -0,0 +1,22 @@
+﻿import sys
+import sqlite3
+
+def main(date):
+    conn = sqlite3.connect('inv.db')
+
+    c = conn.cursor()
+    
+    SQL = "SELECT date, code, fi, it, dl, total from INV where date = \'{}\' order by total asc LIMIT 50".format(date)
+    cur = c.execute(SQL)
+    
+    for row in cur:
+        print(row)
+           
+if __name__ == '__main__':
+    if len(sys.argv) >= 2:
+        date = sys.argv[1]   
+    else:
+        print('Usage: {} DATE'.format(sys.argv[0]))
+        exit()
+        
+    main(date)
